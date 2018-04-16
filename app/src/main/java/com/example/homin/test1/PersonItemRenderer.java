c6ef72ad403e5dbca97a2972368f7fe3d6e5434b
KIMMYSJ <zun710@Hotmail.com>
2018-04-16 15:55:11 +0900

    +사진이미지 resize해서 마커에 추가 +floatingButton으로 자기위치로 카메라줌하는 기능 추가

--- app/src/main/java/com/example/homin/test1/PersonItemRenderer.java
+++ app/src/main/java/com/example/homin/test1/PersonItemRenderer.java
@@ -4,6 +4,13 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -13,6 +20,7 @@ import com.google.maps.android.clustering.ClusterManager;
 import com.google.maps.android.clustering.view.DefaultClusterRenderer;
 import com.google.maps.android.ui.IconGenerator;
 
+//마커 하나하나 이미지와 이름 아이디를 표현해주기 위해 renderer를 상속하는 클래스 생성 !
 public class PersonItemRenderer extends DefaultClusterRenderer<ClusteringMarker> {
     Context context;
     GoogleMap googleMap;
@@ -26,30 +34,50 @@ public class PersonItemRenderer extends DefaultClusterRenderer<ClusteringMarker>
     @Override
     protected void onBeforeClusterItemRendered(ClusteringMarker item, MarkerOptions markerOptions) {
         super.onBeforeClusterItemRendered(item, markerOptions);
-        IconGenerator iconFactory = new IconGenerator(context);
 
-//        markerOptions.title("item");
-//        markerOptions.snippet();
-//
-//        final BitmapFactory.Options options = new BitmapFactory.Options();
-//        options.inJustDecodeBounds = true;
-//        Bitmap bitmap = BitmapFactory.decodeResource(this.context.getResources(),R.drawable.sample_person,options);
-//
-//       Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap,128,128,false);
-//        options.inSampleSize = calculateInSampleSize(options,100,100);
-//
-//        options.inJustDecodeBounds = false;
-//        bitmap = BitmapFactory.decodeResource(this.context.getResources(),R.drawable.sample_person,options);
-//        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(bitmap));
-//
-//
-//        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(iconFactory.makeIcon("hello")));
 
-        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(decodeSampledBitmapFromResource(this.context.getResources(),R.drawable.sample_person, 35, 35)));
+
+       Bitmap rectBitmap =  decodeSampledBitmapFromResource(this.context.getResources(),R.drawable.sample_image, 35, 35); //직사각형 사진
+       Bitmap roundBitmap =  getCircleBitmap(rectBitmap);
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(roundBitmap));
 
 
     }
 
+    //직사각형 비트맵을 원형으로 변환하는 메소드
+    public static Bitmap getCircleBitmap(Bitmap bitmap) {
+        Bitmap output;
+
+        if (bitmap.getWidth() > bitmap.getHeight()) {
+            output = Bitmap.createBitmap(bitmap.getHeight(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        } else {
+            output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getWidth(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        float r = 0;
+
+        if (bitmap.getWidth() > bitmap.getHeight()) {
+            r = bitmap.getHeight() / 2;
+        } else {
+            r = bitmap.getWidth() / 2;
+        }
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawCircle(r, r, r, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+
+    //비트맵 메모리 부족 현상때문에 비트맵 메모리 줄이는 메소드
     public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
                                                          int reqWidth, int reqHeight) {
 
@@ -66,7 +94,7 @@ public class PersonItemRenderer extends DefaultClusterRenderer<ClusteringMarker>
         return BitmapFactory.decodeResource(res, resId, options);
     }
 
-
+    //메모리 부족 현상 때문에 sampleSize를 이용해 가능한 height와 wid를 측정.
     public static int calculateInSampleSize(
                 BitmapFactory.Options options, int reqWidth, int reqHeight) {
             // Raw height and width of image
